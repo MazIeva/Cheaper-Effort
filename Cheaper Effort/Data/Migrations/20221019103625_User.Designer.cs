13e8164de3baff21c632be5d2c7b4f1fd40536ec
popkovaite <90206673+popkovaite@users.noreply.github.com>
2022-10-19 13:37:54 +0300

    errors fixed

--- Cheaper Effort/Data/Migrations/20221019103625_User.Designer.cs
+++ Cheaper Effort/Data/Migrations/20221019103625_User.Designer.cs
@@ -2,6 +2,7 @@
 using Cheaper_Effort.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -9,9 +10,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Cheaper_Effort.Data.Migrations
 {
     [DbContext(typeof(ProjectDbContext))]
-    partial class ProjectDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221019103625_User")]
+    partial class User
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "6.0.10");
--- Cheaper Effort/Pages/Register.cshtml.cs
+++ Cheaper Effort/Pages/Register.cshtml.cs
@@ -29,7 +29,7 @@ namespace Cheaper_Effort.Pages
         }
         public void onGet()
         {
-          
+
         }
 
         public async Task<IActionResult> OnPostAsync()
@@ -38,7 +38,7 @@ namespace Cheaper_Effort.Pages
 
             if (_context.User.Any(o => o.Username == Account.Username || o.Email == Account.Email))
             {
-                ModelState.AddModelError("Usename","Wrong username or email input");
+                ModelState.AddModelError("Usename", "Wrong username or email input");
                 return Page();
             }
 
@@ -46,7 +46,7 @@ namespace Cheaper_Effort.Pages
             {
                 _context.User.Add(Account);
                 _context.SaveChanges();
-                  var claims = new List<Claim> {
+                var claims = new List<Claim> {
                     new Claim(ClaimTypes.Name, Account.Username)
                 };
                 var identity = new ClaimsIdentity(claims, "MyCookieAuth");
@@ -58,6 +58,6 @@ namespace Cheaper_Effort.Pages
             }
 
         }
-        
+
     }
 }

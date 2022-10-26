282deba3cfd5b8cb7ba7990d1f2914fed50a6055
gabijamart <94123391+gabijamart@users.noreply.github.com>
2022-10-26 13:22:00 +0300

    display from database

--- Cheaper Effort/Pages/Forms/Products.cshtml.cs
+++ Cheaper Effort/Pages/Forms/Products.cshtml.cs
@@ -6,68 +6,34 @@ using static Cheaper_Effort.Pages.Forms.ProductsModel;
 using System.Linq;
 using System.Xml.Linq;
 using System.IO;
+using Cheaper_Effort.Data;
+using Cheaper_Effort.Models;
+using Microsoft.Data.SqlClient;
 
 namespace Cheaper_Effort.Pages.Forms
 {
     public class ProductsModel : PageModel
     {
 
-        private readonly ILogger<ProductsModel> _logger;
-        public class Product
+        private readonly ProjectDbContext _context;
+        public ProductsModel(ProjectDbContext context)
         {
-            public int ProductID { get; set; }
-            public string ProductName { get; set; }
-        }
-        [BindProperty]
-        public IList<SelectListItem> Products { get; set; }
-        [TempData]
-        public string SelectedProducts { get; set; }
-        [TempData]
-        public string SelectedProductsIDs { get; set; }
-        public ProductsModel(ILogger<ProductsModel> logger)
-        {
-            _logger = logger;
+            _context = context;
         }
 
-
-        public void OnGet()
+        public async void OnGet()
         {
-            List<Product> ProductList = new List<Product>() {
-                new Product { ProductID = 1, ProductName = "Pasta" },
-                new Product { ProductID = 2, ProductName = "Butter" },
-                new Product { ProductID = 3, ProductName = "Cheese" },
-                new Product { ProductID = 4, ProductName = "Pepper" },
-                new Product { ProductID = 5, ProductName = "Onion" },
-                new Product { ProductID = 6, ProductName = "Curry" },
-                new Product { ProductID = 7, ProductName = "Mayo" },
-                new Product { ProductID = 8, ProductName = "Potato" },
-                new Product { ProductID = 9, ProductName = "Egg" },
-                new Product { ProductID = 10, ProductName = "Flour" },
-                new Product { ProductID = 11, ProductName = "Salt"}
-            };
 
-            Products = ProductList.ToList<Product>().Select(m => new SelectListItem { Text = m.ProductName, Value = m.ProductID.ToString() }).ToList<SelectListItem>();
-
-        }
-        public IActionResult OnPost()
-        {
-            foreach (SelectListItem Product in Products)
+            Ingredients = _context.Ingredients.Select(ingredient => new Ingredient()
             {
-                if (Product.Selected)
-                {
-                    SelectedProducts = $"{Product.Text},{SelectedProducts}";
-                    SelectedProductsIDs = $"{Product.Value},{SelectedProductsIDs}";
-                }
-            }
-            if (SelectedProducts != null) {
-                SelectedProducts = SelectedProducts.TrimEnd(',');
-            SelectedProductsIDs = SelectedProductsIDs.TrimEnd(',');
+                Id = ingredient.Id,
+                IngredientName = ingredient.IngredientName
+            }).ToList();
 
-                //save to file
-                System.IO.File.WriteAllText(@"Data\UserInput\AppFile.txt", this.SelectedProducts.ToString());
-                return RedirectToPage("Products");
         }
-            return RedirectToPage("Products");
-        }
+
+        public IEnumerable<Ingredient> Ingredients { get; set; } = Enumerable.Empty<Ingredient>();
+
     }
+
 }

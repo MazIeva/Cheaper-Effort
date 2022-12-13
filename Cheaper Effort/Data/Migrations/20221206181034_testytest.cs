da67e60486c7ed27b1a53613d3ab70c87041e3d9
popkovaite <90206673+popkovaite@users.noreply.github.com>
2022-12-13 14:42:48 +0200

    one migration

---
+++ Cheaper Effort/Data/Migrations/20221206181034_testytest.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Cheaper_Effort.Data.Migrations
+{
+    public partial class testytest : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Difficult_steps",
+                table: "Recipes",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<double>(
+                name: "Time",
+                table: "Recipes",
+                type: "REAL",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Difficult_steps",
+                table: "Recipes");
+
+            migrationBuilder.DropColumn(
+                name: "Time",
+                table: "Recipes");
+        }
+    }
+}
